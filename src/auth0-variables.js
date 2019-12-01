209c546893e2959659cfbb99e5ceecf4d1d01880
ajeet singh <ajeetrathore11@gmail.com>
2019-12-01 16:46:57 +0530

    add code

---
+++ src/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+    domain: "<YOUR_AUTH0_DOMAIN>",
+    roleUrl: "https://rbac-tutorial-app/role",
+    clientId: "<YOUR_AUTH0_CLIENT_ID>",
+    callbackUrl: "http://localhost:3000/callback"
+  };
